47e61f82cde7187c05f2254244a43217a0dcef03
Daanjbrink <daanjbrink@live.nl>
2018-08-15 16:15:35 +0200

    test

--- listdir.c
+++ listdir.c
@@ -7,15 +7,13 @@ char basedir[]= "./testdir/";
 struct clientData *user;
 
 int callback(const char *filepath, const struct stat *info, const int typeflag, struct FTW *pathinfo){
-
 	send(user->clientfd, filepath, strlen(filepath),0);
 	printf("%s\n",filepath+strlen(basedir));
 	return 0;
 }
 
 char listdir(struct clientData *client){
-	
 	user = client;
 	return nftw(basedir, callback, 15, FTW_PHYS);
-	
+
 }
